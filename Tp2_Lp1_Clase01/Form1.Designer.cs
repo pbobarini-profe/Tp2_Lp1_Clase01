651ba081621096ae9b9331b5562184f2bfc6a270
Solo Profesores <Solo Profesores@DESKTOP-6II5VON>
2025-08-22 20:42:33 -0300

    Agregar archivos de proyecto.

---
+++ Tp2_Lp1_Clase01/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Tp2_Lp1_Clase01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNumero1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbNumero2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.form2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbNumero1
+            // 
+            this.tbNumero1.Location = new System.Drawing.Point(57, 47);
+            this.tbNumero1.Name = "tbNumero1";
+            this.tbNumero1.Size = new System.Drawing.Size(100, 20);
+            this.tbNumero1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbNumero2
+            // 
+            this.tbNumero2.Location = new System.Drawing.Point(188, 46);
+            this.tbNumero2.Name = "tbNumero2";
+            this.tbNumero2.Size = new System.Drawing.Size(100, 20);
+            this.tbNumero2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicacion",
+            "Division",
+            "Factorial Numero 1"});
+            this.comboBox1.Location = new System.Drawing.Point(322, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numero 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.form2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // form2ToolStripMenuItem
+            // 
+            this.form2ToolStripMenuItem.Name = "form2ToolStripMenuItem";
+            this.form2ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.form2ToolStripMenuItem.Text = "Form2";
+            this.form2ToolStripMenuItem.Click += new System.EventHandler(this.form2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tbNumero2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNumero1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumero1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbNumero2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem form2ToolStripMenuItem;
+    }
+}
+
---
+++ Tp2_Lp1_Clase01/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp2_Lp1_Clase01
+{
+    public partial class Form1 : Form
+    {
+        public double resultadoAEnviar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double resultado=0;
+
+            switch (comboBox1.Text)
+            {
+                case "Suma": resultado = double.Parse(tbNumero1.Text) + double.Parse(tbNumero2.Text); ;
+                    break;
+                case "Resta":
+                    resultado = double.Parse(tbNumero1.Text) - double.Parse(tbNumero2.Text); ;
+                    break;
+                case "Multiplicacion":
+                    resultado = double.Parse(tbNumero1.Text) * double.Parse(tbNumero2.Text); ;
+                    break;
+                case "Division":
+                    resultado = double.Parse(tbNumero1.Text) / double.Parse(tbNumero2.Text); ;
+                    break;
+                case "Factorial Numero 1":
+                    resultado = factorial(double.Parse(tbNumero1.Text)) ;
+                    break;
+            }
+            resultadoAEnviar = resultado;
+            MessageBox.Show("El resultado es " + resultado.ToString());
+        }
+
+        private double factorial(double n) { 
+            double resultado = 1;
+            for(int i = 1; i < n+1;i++)
+            {
+                resultado = resultado * i;
+            }
+            return resultado;
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Suma":
+                    label3.Text="+";
+                    break;
+                case "Resta":
+                    label3.Text = "-";
+                    break;
+                case "Multiplicacion":
+                    label3.Text = "x";
+                    break;
+                case "Division":
+                    label3.Text = "%";
+                    break;
+                case "Factorial Numero 1":
+                    tbNumero2.Visible = false;
+                    label2.Visible = false;
+                    break;
+            }
+        }
+
+        private void form2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (resultadoAEnviar != null)
+            {
+                Form2 form2 = new Form2(resultadoAEnviar);
+                form2.Show();
+                this.Hide();
+            }
+        }
+    }
+}
